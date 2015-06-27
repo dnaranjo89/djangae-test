2d4bec59131a4653d81899f7f32b4eba9d5c49f7
David Naranjo <dnaranjo89@gmail.com>
2015-06-27 15:23:34 +0200

    Update select input in index

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,11 @@
 $(document).ready(function () {
+
+    // Submit the query when Select input changes
     $('#category-selector').change(function (){
         $('#filter').submit();
     });
+
+    // Update Select box with the current Category
+    var category_selected = $('#category-selector').attr('data-selected');
+    $("#category-selector").val(category_selected);
 });
